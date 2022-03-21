10b70740f56494ca086b8c627dec4e5f9ac8b6dc
Umesh Chandra Juyal <umeshxz0000@gmail.com>
2022-03-21 17:55:27 +0530

    first file added

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const app = express();
+
+const path= require('path');
+const port= 9000;
+
+// //Builtin Middlewares
+
+// console.log(__dirname);  //path of dir that you working on
+const staicPath = path.join(__dirname ,"../public");
+express.static(staicPath);
+//express.static(path)
+app.use(express.static(staicPath));
+
+//Express app
+//get means to get data from the server
+app.get('/', (req,res)=>{
+    res.send("Welcome to the home page");
+});
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+//   });
+
+//for the listening the server
+app.listen(port,( )=>{
+    console.log(`The express is listening the port no ${port}`);
+})
